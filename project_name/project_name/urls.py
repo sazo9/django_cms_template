f47f108df8706cea0a8bd5a3aa91f16fa01432f7
Felipe cruz <felipecruz@loogica.net>
2013-04-04 11:48:48 -0300

    add login/logout url entries

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import login, logout
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    (r'^login/$', 'django.contrib.auth.views.login',
+                   {'template_name': 'login.html'}),
+    (r'^logout/$', 'django.contrib.auth.views.logout',
+                   {'template_name': 'logged_out.html'}),
     url(r'^$', include('{{ project_name }}.fix.urls'))
 )
