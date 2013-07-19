0dbdb3a893763c265d7378a8e0c8e7f68e13755f
Felipe cruz <felipecruz@loogica.net>
2013-07-19 15:31:25 -0300

    Implement setup_cms using with fabric in Python

--- setup_cms.py
+++ setup_cms.py
@@ -1,7 +1,18 @@
-#!/bin/sh
-PROJECT=$1
-cd $2
-django-admin.py startproject --template=https://github.com/felipecruz/loogica_project_template/archive/master.zip $PROJECT && cd $PROJECT
-make dbinitial
-make migrate
-make runserver
+#!/usr/local/env python
+import os
+import sys
+
+from fabric.operations import local
+from fabric.context_managers import lcd
+
+TEMPLATE_URL = 'https://github.com/felipecruz/loogica_project_template/archive/master.zip'
+DJANGO_CMD = 'django-admin.py startproject --template={template} {project}'
+
+if __name__ == "__main__":
+    project_name = sys.argv[1]
+    with lcd(sys.argv[2]):
+        local(DJANGO_CMD.format(project=project_name,
+                                template=TEMPLATE_URL))
+        with lcd(project_name):
+            local('make dbinitial')
+            local('make runserver')
